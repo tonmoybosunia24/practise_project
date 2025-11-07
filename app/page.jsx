aa5551286c2c76030ccca1590e6160d71b590aca
tonmoybosunia24 <mdtonmoybosunia24@gmail.com>
2025-11-08 00:41:44 +0600

    Basic Setup Ready

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="container">
+      <h1 className="text-primary">Welcome to the Home Page</h1>
+      <button className="button">Book A Call</button>
+    </main>
+  );
+}
