d1e482d63cf65a516f9d883e68d326617d66243d
tonmoybosunia24 <mdtonmoybosunia24@gmail.com>
2025-11-09 18:57:16 +0600

    Font Setup Ready

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,18 +1,8 @@
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Archivo } from "next/font/google";
 import '@/styles/globals.css';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-const inter = Inter({
-  variable: "--font-inter",
+const archivo = Archivo({
+  variable: "--font-archivo",
   subsets: ["latin"],
   weights: ["400", "600", "700"],  
   display: "swap",
@@ -27,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${inter.variable} antialiased`}>
+        className={`${archivo.variable} antialiased`}>
         {children}
       </body>
     </html>
